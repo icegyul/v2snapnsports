e54febbd316dbdc07b209957cfb3975c79538321
icegyul <icegyul99@gmail.com>
2026-08-29 12:21:48 +0900

    fix: remove photo plate from stadium renderer

--- apps/web/src/three/stadiumWebgl.ts
+++ apps/web/src/three/stadiumWebgl.ts
@@ -1,6 +1,5 @@
 import "../features/stadium/stadiumCinematic.css";
 import "../features/stadium/stadiumFinal.css";
 import "../features/stadium/stadiumPremiumTuning.css";
-import "../features/stadium/stadiumPhotorealHybrid.css";
 export { createStadiumWebglRenderer } from "./stadiumWebglPremiumV2";
 export type { StadiumWebglRenderer } from "./stadiumWebglPremiumV2";
