95b57b045a5605b946856bed16beb0be6bc35334
icegyul <icegyul99@gmail.com>
2026-08-29 15:35:42 +0900

    feat: enable stadium v6.2 mobile framing

--- apps/web/src/three/stadiumWebgl.ts
+++ apps/web/src/three/stadiumWebgl.ts
@@ -2,5 +2,6 @@ import "../features/stadium/stadiumCinematic.css";
 import "../features/stadium/stadiumFinal.css";
 import "../features/stadium/stadiumPremiumTuning.css";
 import "../features/stadium/stadiumV61Framing.css";
+import "../features/stadium/stadiumV62Mobile.css";
 export { createStadiumWebglRenderer } from "./stadiumWebglV6";
 export type { StadiumWebglRenderer } from "./stadiumWebglV6";
