9ca405352adbad4cf1a81223010e40ed1b1d907f
icegyul <icegyul99@gmail.com>
2026-08-29 14:49:41 +0900

    feat: switch stadium home to v4 renderer

--- apps/web/src/three/stadiumWebgl.ts
+++ apps/web/src/three/stadiumWebgl.ts
@@ -1,5 +1,5 @@
 import "../features/stadium/stadiumCinematic.css";
 import "../features/stadium/stadiumFinal.css";
 import "../features/stadium/stadiumPremiumTuning.css";
-export { createStadiumWebglRenderer } from "./stadiumWebglV3";
-export type { StadiumWebglRenderer } from "./stadiumWebglV3";
+export { createStadiumWebglRenderer } from "./stadiumWebglV4";
+export type { StadiumWebglRenderer } from "./stadiumWebglV4";
