a681afc9865730a63a1fc246b7baa1480a830934
icegyul <icegyul99@gmail.com>
2026-08-29 10:20:35 +0900

    feat: switch stadium to cinematic renderer

--- apps/web/src/three/stadiumWebgl.ts
+++ apps/web/src/three/stadiumWebgl.ts
@@ -1,3 +1,3 @@
 import "../features/stadium/stadiumCinematic.css";
-export { createStadiumWebglRenderer } from "./stadiumWebglPro";
-export type { StadiumWebglRenderer } from "./stadiumWebglPro";
+export { createStadiumWebglRenderer } from "./stadiumWebglCinematic";
+export type { StadiumWebglRenderer } from "./stadiumWebglCinematic";
