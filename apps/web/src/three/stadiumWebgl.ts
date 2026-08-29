4b2f4ac5f5a0e328651336e9d828f92f04fe7b4d
icegyul <icegyul99@gmail.com>
2026-08-29 10:57:13 +0900

    feat: apply final stadium matchday presentation

--- apps/web/src/three/stadiumWebgl.ts
+++ apps/web/src/three/stadiumWebgl.ts
@@ -1,3 +1,4 @@
 import "../features/stadium/stadiumCinematic.css";
+import "../features/stadium/stadiumFinal.css";
 export { createStadiumWebglRenderer } from "./stadiumWebglFinal";
 export type { StadiumWebglRenderer } from "./stadiumWebglFinal";
