6eb0c56e6f35712820bd90308cbfe976588f3050
icegyul <icegyul99@gmail.com>
2026-08-29 16:09:27 +0900

    feat: enable stadium v7.5 presentation

--- apps/web/src/three/stadiumWebgl.ts
+++ apps/web/src/three/stadiumWebgl.ts
@@ -8,5 +8,6 @@ import "../features/stadium/stadiumV64Crop.css";
 import "../features/stadium/stadiumV65Viewport.css";
 import "../features/stadium/stadiumV7Live.css";
 import "../features/stadium/stadiumV74Presentation.css";
+import "../features/stadium/stadiumV75Presentation.css";
 export { createStadiumWebglRenderer } from "./stadiumWebglV7";
 export type { StadiumWebglRenderer } from "./stadiumWebglV7";
