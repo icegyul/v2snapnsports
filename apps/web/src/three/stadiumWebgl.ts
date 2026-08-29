412b76c7cbc98c95d30e7270c26bc69aef4681c8
icegyul <icegyul99@gmail.com>
2026-08-29 16:13:55 +0900

    feat: enable stadium v7.6 lighting pass

--- apps/web/src/three/stadiumWebgl.ts
+++ apps/web/src/three/stadiumWebgl.ts
@@ -9,5 +9,6 @@ import "../features/stadium/stadiumV65Viewport.css";
 import "../features/stadium/stadiumV7Live.css";
 import "../features/stadium/stadiumV74Presentation.css";
 import "../features/stadium/stadiumV75Presentation.css";
+import "../features/stadium/stadiumV76Lighting.css";
 export { createStadiumWebglRenderer } from "./stadiumWebglV7";
 export type { StadiumWebglRenderer } from "./stadiumWebglV7";
