f13b86386987f32ba8083cdeff70db64b8391046
icegyul <icegyul99@gmail.com>
2026-08-29 15:32:35 +0900

    feat: enable stadium v6.1 immersive framing

--- apps/web/src/three/stadiumWebgl.ts
+++ apps/web/src/three/stadiumWebgl.ts
@@ -1,5 +1,6 @@
 import "../features/stadium/stadiumCinematic.css";
 import "../features/stadium/stadiumFinal.css";
 import "../features/stadium/stadiumPremiumTuning.css";
+import "../features/stadium/stadiumV61Framing.css";
 export { createStadiumWebglRenderer } from "./stadiumWebglV6";
 export type { StadiumWebglRenderer } from "./stadiumWebglV6";
