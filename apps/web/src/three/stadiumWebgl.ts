85c93a1bcf3ef3d45e34e0260a97421c6d9a5f17
icegyul <icegyul99@gmail.com>
2026-08-29 15:39:35 +0900

    feat: enable stadium v6.3 polish

--- apps/web/src/three/stadiumWebgl.ts
+++ apps/web/src/three/stadiumWebgl.ts
@@ -3,5 +3,6 @@ import "../features/stadium/stadiumFinal.css";
 import "../features/stadium/stadiumPremiumTuning.css";
 import "../features/stadium/stadiumV61Framing.css";
 import "../features/stadium/stadiumV62Mobile.css";
+import "../features/stadium/stadiumV63Polish.css";
 export { createStadiumWebglRenderer } from "./stadiumWebglV6";
 export type { StadiumWebglRenderer } from "./stadiumWebglV6";
