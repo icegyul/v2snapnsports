779b658f1f2f6643cd559fa603376da7e4533040
icegyul <icegyul99@gmail.com>
2026-08-29 11:22:33 +0900

    feat: apply premium matchday grading

--- apps/web/src/three/stadiumWebgl.ts
+++ apps/web/src/three/stadiumWebgl.ts
@@ -1,4 +1,5 @@
 import "../features/stadium/stadiumCinematic.css";
 import "../features/stadium/stadiumFinal.css";
+import "../features/stadium/stadiumPremiumTuning.css";
 export { createStadiumWebglRenderer } from "./stadiumWebglPremium";
 export type { StadiumWebglRenderer } from "./stadiumWebglPremium";
