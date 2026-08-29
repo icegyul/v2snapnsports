7928a6a0e11385bcb4b335e0049ad2aae210d9a8
icegyul <icegyul99@gmail.com>
2026-08-29 18:08:50 +0900

    feat: switch stadium first screen to v10 physical renderer

--- apps/web/src/three/stadiumWebgl.ts
+++ apps/web/src/three/stadiumWebgl.ts
@@ -13,5 +13,5 @@ import "../features/stadium/stadiumV76Lighting.css";
 import "../features/stadium/stadiumV77Showcase.css";
 import "../features/stadium/stadiumV84MobileFinish.css";
 import "../features/stadium/stadiumV93MobileImmersive.css";
-export { createStadiumWebglRenderer } from "./stadiumWebglV83";
-export type { StadiumWebglRenderer } from "./stadiumWebglV83";
+export { createStadiumWebglRenderer } from "./stadiumWebglV10";
+export type { StadiumWebglRenderer } from "./stadiumWebglV10";
