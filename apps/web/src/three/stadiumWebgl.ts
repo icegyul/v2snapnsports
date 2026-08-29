c8a8a798735112b7937ff7886d12fb2462dfef72
icegyul <icegyul99@gmail.com>
2026-08-29 15:18:32 +0900

    feat: switch stadium home to cinematic v5 camera

--- apps/web/src/three/stadiumWebgl.ts
+++ apps/web/src/three/stadiumWebgl.ts
@@ -1,5 +1,5 @@
 import "../features/stadium/stadiumCinematic.css";
 import "../features/stadium/stadiumFinal.css";
 import "../features/stadium/stadiumPremiumTuning.css";
-export { createStadiumWebglRenderer } from "./stadiumWebglV4";
-export type { StadiumWebglRenderer } from "./stadiumWebglV4";
+export { createStadiumWebglRenderer } from "./stadiumWebglV5";
+export type { StadiumWebglRenderer } from "./stadiumWebglV5";
