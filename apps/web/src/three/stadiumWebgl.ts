a30ba4fb99f715a2bcf51290568f82b5cc27942a
icegyul <icegyul99@gmail.com>
2026-08-29 11:27:25 +0900

    feat: activate premium v2 stadium materials

--- apps/web/src/three/stadiumWebgl.ts
+++ apps/web/src/three/stadiumWebgl.ts
@@ -1,5 +1,5 @@
 import "../features/stadium/stadiumCinematic.css";
 import "../features/stadium/stadiumFinal.css";
 import "../features/stadium/stadiumPremiumTuning.css";
-export { createStadiumWebglRenderer } from "./stadiumWebglPremium";
-export type { StadiumWebglRenderer } from "./stadiumWebglPremium";
+export { createStadiumWebglRenderer } from "./stadiumWebglPremiumV2";
+export type { StadiumWebglRenderer } from "./stadiumWebglPremiumV2";
