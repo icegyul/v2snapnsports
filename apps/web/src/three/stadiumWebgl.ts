675a8b9492967b18bebeeead5bffde6219b8bc65
icegyul <icegyul99@gmail.com>
2026-08-29 17:54:17 +0900

    feat: enable stadium v9.3 immersive mobile presentation

--- apps/web/src/three/stadiumWebgl.ts
+++ apps/web/src/three/stadiumWebgl.ts
@@ -12,5 +12,6 @@ import "../features/stadium/stadiumV75Presentation.css";
 import "../features/stadium/stadiumV76Lighting.css";
 import "../features/stadium/stadiumV77Showcase.css";
 import "../features/stadium/stadiumV84MobileFinish.css";
+import "../features/stadium/stadiumV93MobileImmersive.css";
 export { createStadiumWebglRenderer } from "./stadiumWebglV83";
 export type { StadiumWebglRenderer } from "./stadiumWebglV83";
