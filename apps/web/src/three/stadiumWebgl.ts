a8ae80d0e67cc86e089d56f9fb51c57045f64312
icegyul <icegyul99@gmail.com>
2026-08-29 11:36:12 +0900

    feat: activate photoreal hybrid stadium presentation

--- apps/web/src/three/stadiumWebgl.ts
+++ apps/web/src/three/stadiumWebgl.ts
@@ -1,5 +1,6 @@
 import "../features/stadium/stadiumCinematic.css";
 import "../features/stadium/stadiumFinal.css";
 import "../features/stadium/stadiumPremiumTuning.css";
+import "../features/stadium/stadiumPhotorealHybrid.css";
 export { createStadiumWebglRenderer } from "./stadiumWebglPremiumV2";
 export type { StadiumWebglRenderer } from "./stadiumWebglPremiumV2";
