5b8e9b45dfe7a90d0cc81376b05ae3022e4dd5ae
icegyul <icegyul99@gmail.com>
2026-08-29 19:27:36 +0900

    feat: switch first screen to v11 physical base stadium

--- apps/web/src/three/stadiumWebgl.ts
+++ apps/web/src/three/stadiumWebgl.ts
@@ -13,5 +13,5 @@ import "../features/stadium/stadiumV76Lighting.css";
 import "../features/stadium/stadiumV77Showcase.css";
 import "../features/stadium/stadiumV84MobileFinish.css";
 import "../features/stadium/stadiumV93MobileImmersive.css";
-export { createStadiumWebglRenderer } from "./stadiumWebglV101";
-export type { StadiumWebglRenderer } from "./stadiumWebglV101";
+export { createStadiumWebglRenderer } from "./stadiumWebglV11";
+export type { StadiumWebglRenderer } from "./stadiumWebglV11";
